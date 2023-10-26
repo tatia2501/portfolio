08c222d5b3099ddd5c7f9c468378a9e3378b312c
tatia2501 <catatat@yandex.ru>
2023-10-26 11:42:04 +0300

    projects adding

--- script.js
+++ script.js
@@ -3,16 +3,6 @@ const kokos_template = document.getElementById('kokos_template');
 const kokos_down_btn = document.getElementById('kokos_down_btn');
 const kokos_up_btn = document.getElementById('kokos_up_btn');
 
-const quest_container = document.getElementById('quest_container');
-const quest_template = document.getElementById('quest_template');
-const quest_down_btn = document.getElementById('quest_down_btn');
-const quest_up_btn = document.getElementById('quest_up_btn');
-
-const tutor_container = document.getElementById('tutor_container');
-const tutor_template = document.getElementById('tutor_template');
-const tutor_down_btn = document.getElementById('tutor_down_btn');
-const tutor_up_btn = document.getElementById('tutor_up_btn');
-
 kokos_down_btn.addEventListener('click', async () => {
     kokos_down_btn.style.visibility = 'hidden';
     kokos_up_btn.style.visibility = 'visible';
@@ -30,6 +20,11 @@ kokos_up_btn.addEventListener('click', async () => {
     kokos_container.innerHTML = '';
 })
 
+const quest_container = document.getElementById('quest_container');
+const quest_template = document.getElementById('quest_template');
+const quest_down_btn = document.getElementById('quest_down_btn');
+const quest_up_btn = document.getElementById('quest_up_btn');
+
 quest_down_btn.addEventListener('click', async () => {
     quest_down_btn.style.visibility = 'hidden';
     quest_up_btn.style.visibility = 'visible';
@@ -46,6 +41,11 @@ quest_up_btn.addEventListener('click', async () => {
     quest_down_btn.style.visibility = 'visible';
     quest_container.innerHTML = '';
 })
+
+const tutor_container = document.getElementById('tutor_container');
+const tutor_template = document.getElementById('tutor_template');
+const tutor_down_btn = document.getElementById('tutor_down_btn');
+const tutor_up_btn = document.getElementById('tutor_up_btn');
 
 tutor_down_btn.addEventListener('click', async () => {
     tutor_down_btn.style.visibility = 'hidden';
